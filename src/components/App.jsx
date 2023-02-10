6308bdd339443d470cb2e0efc79733c3b7fef0ff
Hanna Tubolets <anna.tubolets@gmail.com>
2023-02-10 09:12:34 +0300

    Updated App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,20 @@
 import { Component } from 'react';
-import {FeedbackOptions} from './FeedbackOptions/FeedbackOptions';
-import {Statistics} from './Statistics/Statistics';
-import {Section} from './Section/Section';
-import {Notification} from './Notification/Notification';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
 
 export class App extends Component {
   render() {
     return (
       <>
-          <Section title="Please leave feedback">
+        <Section title="Please leave feedback">
           <FeedbackOptions />
         </Section>
-        <Section>
+        <Section title="Statistics">
           <Statistics></Statistics>
-          <Notification message="No feedback given"/>
+          <Notification message="No feedback given" />
         </Section>
-
-     
       </>
     );
   }
